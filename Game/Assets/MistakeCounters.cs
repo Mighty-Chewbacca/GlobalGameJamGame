41ceabc3480b34cee1fe4dc73869e394329cf204
Elliot Pryde <elliotpryde@hotmail.co.uk>
2015-01-24 03:15:14 +0000

    Counters started

---
+++ Game/Assets/MistakeCounters.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MistakeCounters : MonoBehaviour {
+
+    byte BedroomCounter1, BedroomCounter2, StairsCounter, CorridorCounter1,
+        CorridorCounter2, KitchenCounter1, KitchenCounter2, FrontdoorCounter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // if you fail a task, increment the respective counter
+	}
+}
