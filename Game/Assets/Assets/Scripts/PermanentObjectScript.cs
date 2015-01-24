02ab3005f86f7a025e87186fe7acc8404761effb
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2015-01-24 08:50:02 +0000

    scene switching beginnings

---
+++ Game/Assets/Assets/Scripts/PermanentObjectScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PermanentObjectScript : MonoBehaviour {
+
+	//this script is going to permenantly going to be in the game
+	//created in the menu and kept throughout
+
+
+
+	void awake()
+	{
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
