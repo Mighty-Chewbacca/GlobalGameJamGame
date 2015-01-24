b66f5e54490562ef3e55c999a9951c5267fa9df6
Elliot Pryde <elliotpryde@hotmail.co.uk>
2015-01-24 03:06:02 +0000

    Basic shaking

---
+++ Game/Assets/ShakeSprite.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakeSprite : MonoBehaviour {
+
+    public float shakeSpeed = 0.05f;
+    bool i, o;
+	// Use this for initialization
+	void Start () {
+        i = true;
+        o = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (o)
+        {
+            this.GetComponent<Transform>().Translate(new Vector3(
+                i == true ? shakeSpeed : -shakeSpeed,
+                i == true ? shakeSpeed : -shakeSpeed,
+                0.0f));
+        }
+        else
+        {
+            this.GetComponent<Transform>().Translate(new Vector3(
+                i == true ? -shakeSpeed : shakeSpeed,
+                i == true ? shakeSpeed : -shakeSpeed,
+                0.0f));
+
+            i = !i;
+        }
+        o = !o;
+	}
+}
